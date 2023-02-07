f48d4121ffb53027d19291318a770c2675c19193
Forslunddavid <forslund.david@gmail.com>
2023-02-07 15:26:53 +0100

    stat table

--- Script/Button-click.js
+++ Script/Button-click.js
@@ -19,36 +19,60 @@ button.addEventListener("click", function() {
 
 // overlay statistik
 
-const statisticsAnchor = document.querySelector(".statistics");
+const statistics = document.querySelector(".statistics");
 
-statisticsAnchor.addEventListener("click", function() {
+statistics.addEventListener("click", function() {
 
-// styla overlay
-  const overlay = document.createElement("div");
-  overlay.style.position = "fixed";
-  overlay.style.top = 0;
-  overlay.style.left = 0;
-  overlay.style.width = "100%";
-  overlay.style.height = "100%";
-  overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-  overlay.style.display = "flex";
-  overlay.style.alignItems = "center";
-  overlay.style.justifyContent = "center";
+// skapa / styla overlay
+	const overlay = document.createElement("div");
+	overlay.style.position = "fixed";
+	overlay.style.top = 0;
+	overlay.style.left = 0;
+	overlay.style.width = "100%";
+	overlay.style.height = "100%";
+	overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+	overlay.style.display = "flex";
+	overlay.style.alignItems = "center";
+	overlay.style.justifyContent = "center";
 
-  // Create the content element
-  const content = document.createElement("div");
-  content.style.backgroundColor = "#fff";
-  content.style.padding = "20px";
-  content.style.borderRadius = "10px";
-  content.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
-  content.style.width = "90vw";
-  content.style.height = "90vh";
-  content.style.zIndex = "30";
-  content.textContent = "Statistik";
+  // skapa / styla på content
+	const content = document.createElement("section");
+	content.style.backgroundColor = "#fff";
+	content.style.padding = "20px";
+	content.style.borderRadius = "10px";
+	content.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
+	content.style.width = "90vw";
+	content.style.height = "90vh";
+	content.style.zIndex = "30";
+	content.textContent = "Statistik";
 
-  // Append the content to the overlay
-  overlay.appendChild(content);
+	const table = document.createElement("table");
 
-  // Append the overlay to the body
-  document.body.appendChild(overlay);
+	const row = document.createElement("tr");
+
+
+// Append the content to the overlay
+	overlay.appendChild(content);
+
+// Append the overlay to the body
+	document.body.appendChild(overlay);
+
+// Append the table to the overlay
+	content.appendChild(table);
+
+// Append the table row to the table
+    table.appendChild(row);
+
+//Append the table header to the table row
+    row.appendChild(document.createElement("th"));
+
+// Append the table data to the table row
+    row.appendChild(document.createElement("td"));
+
+	overlay.addEventListener("click", function(event) {
+    if (event.target === overlay) {
+    overlay.remove();
+    }
+});
+
 });

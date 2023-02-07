c9ce132a566b611c664c5514735cc51c9bf61681
Abbe <abdullah.abou.shihab@icloud.com>
2023-02-07 14:25:07 +0100

    update

--- Script/script.js
+++ Script/script.js
@@ -32,13 +32,12 @@ const wordList = [
   'square',
 ];
 
-document.getElementById("myButton").onclick =  function(){
-  var myName = document.getElementById("myText").value;
-  console.log("Hello", myName);
-}
+// document.getElementById("myButton").onclick =  function(){
+//   var myName = document.getElementById("myText").value;
+//   console.log("Hello", myName);
+// }
 
 // var myName = window.prompt("What is your name?")
-
 // console.log("Hello", myName);
 
 

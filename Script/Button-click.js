a26cc3352212c4ab19075084b2855d1bf6b9b359
QueAnka <annika.nylin@hotmail.com>
2023-02-11 16:12:24 +0100

    skapat overlays etc.

--- Script/Button-click.js
+++ Script/Button-click.js
@@ -1,10 +1,21 @@
 // skapa referens till knappen
-const playbutton = document.querySelector("#button");
+const playButton = document.querySelector("#button");
 const playbutton2 = document.querySelector("#button");
-
+const nameInput = document.querySelector('.player-name')
 // lägg till en eventlistener för att klicka på knappen
 
-playbutton.addEventListener("click", function () {
+nameInput.addEventListener('keydown', function (event) {
+	event.stopPropagation();
+});
+nameInput.addEventListener('input', function () {
+	playButton.disabled =(this.value ==='');
+});
+
+
+
+playButton.addEventListener("click", function () {
+
+
 
 	// välj alla divar med stil invisible
 
@@ -49,47 +60,27 @@ const statistics = document.querySelector(".statistics");
 statistics.addEventListener("click", function () {
 
 	// skapa / styla overlay
-	const overlay = document.createElement("div");
-	overlay.style.position = "fixed";
-	overlay.style.top = 0;
-	overlay.style.left = 0;
-	overlay.style.width = "100%";
-	overlay.style.height = "100%";
-	overlay.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-	overlay.style.display = "flex";
-	overlay.style.alignItems = "center";
-	overlay.style.justifyContent = "center";
-
+	const statisticsOverlay = document.createElement("div");
+	statisticsOverlay.className = ('overlay')
+	
 	// skapa / styla på content
-	const content = document.createElement("section");
-	content.style.backgroundColor = "#ffffff";
-	content.style.padding = "20px";
-	content.style.borderRadius = "10px";
-	content.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.5)";
-	content.style.width = "90vw";
-	content.style.height = "90vh";
-	content.style.zIndex = "30";
-	content.textContent = "Statistik";
-
+	const statisticsContent = document.createElement("section");
+	statisticsContent.className = ('statistics-content')
 
 	// Append the content to the overlay
-	overlay.appendChild(content);
+	statisticsOverlay.appendChild(statisticsContent);
 
 	// Append the overlay to the body
-	document.body.appendChild(overlay);
+	document.body.appendChild(statisticsOverlay);
 
 
 
-	overlay.addEventListener("click", function (event) {
-		if (event.target === overlay) {
-			overlay.remove();
+	statisticsOverlay.addEventListener("click", function (event) {
+		if (event.target === statisticsOverlay) {
+			statisticsOverlay.remove();
 		}
 	});
 
 });
 
 
-const nameInput = document.querySelector('.player-name')
-nameInput.addEventListener('keydown', function (event) {
-	event.stopPropagation();
-});
--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,11 @@
 import { words } from "./ord.js"
-import{} from './functions.js'
-import{} from '/local-storage.js'
+
+
+// testa toogle för visability på hangman-delarna
+// ngn form av counter kopplad
+// buttons få dem att tömma och starta nytt spel utan refresh, function som använder RandomValue
+// hämta info från LS och skriva ut den i statistik och sortera
+
 
 // variabler för själva spelet
 const randomWords = words
@@ -20,7 +25,102 @@ const wrongAnswer = []
 console.log(randomValueArray)
 console.log(wrongAnswer)
 
+
+// funktioner
+
+makeWrongLetterArray()
+
+function makeWrongLetterArray() {
+	for (let j = 0; j < 6; j++) {
+		wrongAnswer.push("")
+	}
+}
+
+function renderWrongLetter(letter) {
+	let wrongLetter = document.createElement('p')
+	wrongLetter.innerText = (letter)
+	wrongLetter.className = ('wrong-letter-text')
+
+	wrongLetterContainer.insertAdjacentElement('afterbegin', wrongLetter);
+	// console.log(wrongLetter)
+}
+
+function renderHangman(){
+
+}
+
+
+
+
+makeAnswerArray();
+
+function makeAnswerArray() {
+	for (let i = 0; i < randomValueArray.length; i++) {
+		answer.push("");
+	}
+	// console.log(answer);
+}
+
+
+renderLines();
+
+function renderLines() {
+	guessWord.innerHTML = "";
+	for (let lines = 0; lines < answer.length; lines++) {
+		const underLine = document.createElement('p')
+		underLine.innerText = (answer[lines])
+		underLine.className = ('under-line')
+
+		guessWord.append(underLine)
+
+	}
+}
+
+// funktioner för att skapa overlays
+
+function createWinOverlay(){
+	const winOverlay = document.createElement("div");
+	const winButton = document.createElement('button')
+	const winContent = document.createElement('p');
+	
+	winOverlay.className = ('overlay')
+	winButton.className = ('new-game-button')
+	winButton.innerText = ('Nytt spel')
+	winContent.innerText = ('Grattis du vann! Du gissade ' + guessedLetters.length + ' gånger')
+	winContent.className = ('content')
+	
+		// Append the content to the overlay
+		winOverlay.appendChild(winContent);
+		winContent.appendChild(winButton);
+	
+		// Append the overlay to the body
+		document.body.appendChild(winOverlay);
+	}
+	
+		
+	function createLooseOverlay(){
+		const looseOverlay = document.createElement("div");
+		const looseButton = document.createElement('button')
+		const looseContent = document.createElement('p')
+	
+		looseOverlay.className = ('overlay')
+		looseButton.className = ('new-game-button')
+		looseButton.innerText = ('Nytt spel')
+		looseContent.innerText = ('Tyvärr du förlorade! Det rätta order var: ' + randomValue)
+		looseContent.className = ('content')
+	
+			// Append the content to the overlay
+			looseOverlay.appendChild(looseContent);
+			looseContent.appendChild(looseButton);
+		
+		
+			// Append the overlay to the body
+			document.body.appendChild(looseOverlay);
+	}
+
+
 // kod för att gissa bokstäver, spara rätt/fel
+
 document.addEventListener('keydown', (event) => {
 	let correctWordAsArray = randomValueArray;
 	let guessedLetter = event.key;
@@ -43,10 +143,11 @@ document.addEventListener('keydown', (event) => {
 			renderLines();
 			console.log(correctWordAsArray, answer);
 
+			// när man vinner
 			if (JSON.stringify(correctWordAsArray) == JSON.stringify(answer)) {
-				alert('du vann');
+				createWinOverlay()
 				// spara undan spelarens resultat i LS
-				saveUserData(guessedLetters.length, true);
+				saveUserData(wrongLetters.length, true);
 			}
 		}
 		else {
@@ -56,20 +157,107 @@ document.addEventListener('keydown', (event) => {
 				guessedLetters.push(guessedLetter);
 				wrongLetters.push(guessedLetter);
 				wrongLetterContainer.innerHTML = "";
+
+
 				for (let y = 0; y < wrongLetters.length; y++) {
 					console.log(wrongLetters[y]);
 					wrongAnswer.unshift(guessedLetter);
 					renderWrongLetter(wrongLetters[y]);
+					
+					if(wrongLetters.length = 1){
+						drawing[0].style.visability = visible
+					}
 				}
-				if (wrongLetters.length === 5) {
-					alert('game over');
+
+				// när man förlorar
+				if (wrongLetters.length === 6) {
+					createLooseOverlay()
 					// spara undan spelarens resultat i LS
-					saveUserData(guessedLetters.length, false);
+					saveUserData(wrongLetters.length, false);
 				}
 			}
 		}
 	}
 })
+
+
+
+
+
+
+if(wrongLetters.length = 1){
+	drawing[0].style.visability = visible
+}
+
+	const drawing = [ground, scaffold, head, body, arms, legs]
+    ground = document.querySelector('#ground'), 
+    scaffold = document.querySelector('#scaffold'),
+    legs = document.querySelector('#legs'),
+    arms = document.querySelector('#arms'),
+    body = document.querySelector('#body'),
+    head = document.querySelector('#head')
+
+
+// const ground = hangman.ground
+// const scaffold = hangman.scaffold
+// const head = hangman.head
+// const body = hangman.body
+// const arms = hangman.arms
+// const legs = hangman.legs
+
+
+
+// ground.style.display = invisible
+// scaffold.style.display = invisible
+// head.style.display = invisible
+// body.style.display = invisible
+// arms.style.display = invisible
+// legs.style.display = invisible
+
+
+/* function incorrectGuess() {
+	const drawing = [ground, scaffold, head, body, arms, legs]
+	
+	// rita ut gubben vid felsvar
+	if (shuffle.includes (event. key) == false && onlyLetter. includes (event. key) == true) {
+	drawing[countWrongAnswer].style.display = visible
+	countWrongAnswer++
+	console. log('incorrect guess')
+	}
+}
+	 */
+
+
+
+
+
+
+
+// initialize local storage with empty array of users
+let users = [];
+console.log('hej');
+localStorage.setItem('users', JSON.stringify(users));
+
+function saveUserData(score, won) {
+	let users = JSON.parse(localStorage.getItem('users'));
+	let currentUser = JSON.parse(localStorage.getItem('currentUser'));
+
+	if (won) {
+		currentUser.result = 'Vinst'
+	} else {
+		currentUser.result = 'Förlust'
+	}
+	currentUser.score = score;
+
+	users.push(currentUser);
+	localStorage.setItem('users', JSON.stringify(users));
+}
+
+
+
+
+
+
 
 
 

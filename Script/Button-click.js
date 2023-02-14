04943c26e7974e420be4648907af9a9e4152c0a9
Forslunddavid <forslund.david@gmail.com>
2023-02-14 12:20:20 +0100

    sortering funkar

--- Script/Button-click.js
+++ Script/Button-click.js
@@ -36,7 +36,7 @@ playButton.addEventListener("click", function () {
 
 playbutton2.addEventListener("click", function () {
 
-	// ta namnet från input och spara i Local Storage
+// ta namnet från input och spara i Local Storage
 	let newUserName = document.querySelector('.player-name').value;
 	let currentUser = {
 		name: newUserName,
@@ -46,10 +46,10 @@ playbutton2.addEventListener("click", function () {
 
 	localStorage.setItem('currentUser', JSON.stringify(currentUser));
 
-	// välj alla divar med stil welcome
+// välj alla divar med stil welcome
 	const divs = document.querySelectorAll(".welcome");
 	console.log('click', divs)
-	// Dölj visa div
+// Dölj visa div
 
 	divs.forEach(function (div) {
 		div.classList.toggle('welcome-hidden');
@@ -59,41 +59,166 @@ playbutton2.addEventListener("click", function () {
 
 
 
-// overlay statistik
+// // overlay statistik
+// const statistics = document.querySelector(".statistics");
+
+// statistics.addEventListener("click", function () {
+
+// // skapa / styla overlay
+// 	const statisticsOverlay = document.createElement("div");
+// 	statisticsOverlay.className = ('overlay');
+
+// // skapa / styla  content
+// 	const statisticsContent = document.createElement("section");
+// 	statisticsContent.className = ('statistics-content');
+
+// // skapa / styla rubrik
+// 	const statisticsHeader = document.createElement("h2");
+// 	statisticsHeader.className = ('statistics-header');
+// 	statisticsHeader.innerText = ('Statistik');
+
+// // skapa / styla sortering
+// 	const statisticsSorterName = document.createElement("a");
+// 	statisticsSorterName.className = ('statistics-sorter');
+//     statisticsSorterName.href = '#';
+// 	statisticsSorterName.innerText = ('Namn');
+
+
+// 	const statisticsSorterPoints = document.createElement("a");
+// 	statisticsSorterPoints.className = ('statistics-sorter');
+// 	statisticsSorterPoints.href = '#';
+// 	statisticsSorterPoints.innerText = ('Poäng');
+
+// 	const menuSection = document.createElement("section");
+// 	menuSection.className = ('menu-section')
+
+
+
+// // Lägg till overlay till body
+// 	document.body.appendChild(statisticsOverlay);
+
+// // Lägg till content till overlay
+// 	statisticsOverlay.appendChild(statisticsContent);
+
+// //lägg till innehåll i content
+
+// 	statisticsContent.appendChild(statisticsHeader);
+// 	statisticsContent.appendChild(menuSection);
+// 	menuSection.appendChild(statisticsSorterName);
+// 	menuSection.appendChild(statisticsSorterPoints);
+
+
+
+// // Lägg till listan till content
+// 	let users = JSON.parse(localStorage.getItem('users'));
+// 	let userList = document.createElement('ul');
+// 	userList.className = 'user-list';
+// 	users.forEach(user => {
+//     let userItem = document.createElement('li');
+// 	userItem.className = 'user-item';
+//     userItem.textContent = `${user.name} - Score: ${user.score}, Result: ${user.result}`;
+//     userList.appendChild(userItem);
+// 	});
+// 	statisticsContent.appendChild(userList);
+
+// 	statisticsOverlay.addEventListener("click", function (event) {
+//     if (event.target === statisticsOverlay) {
+//     	statisticsOverlay.remove();
+//     }
+// 	});
+
+// });
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////
 const statistics = document.querySelector(".statistics");
+let users = JSON.parse(localStorage.getItem('users'));
 
 statistics.addEventListener("click", function () {
 
-  // skapa / styla overlay
-	const statisticsOverlay = document.createElement("div");
-	statisticsOverlay.className = ('overlay');
+    // Skapa overlay och content
+    const statisticsOverlay = document.createElement("div");
+    statisticsOverlay.className = ('overlay');
+    const statisticsContent = document.createElement("section");
+    statisticsContent.className = ('statistics-content');
 
-  // skapa / styla  content
-	const statisticsContent = document.createElement("section");
-	statisticsContent.className = ('statistics-content');
+    // skapa Header
+    const statisticsHeader = document.createElement("h2");
+    statisticsHeader.className = ('statistics-header');
+    statisticsHeader.innerText = ('Statistik');
 
-  // Lägg till content till overlay
-	statisticsOverlay.appendChild(statisticsContent);
+    // skapa sorteringslänkar
+    const statisticsSorterName = document.createElement("a");
+    statisticsSorterName.className = ('statistics-sorter');
+    statisticsSorterName.href = '#';
+    statisticsSorterName.innerText = ('Senast');
+    const statisticsSorterPoints = document.createElement("a");
+    statisticsSorterPoints.className = ('statistics-sorter');
+    statisticsSorterPoints.href = '#';
+    statisticsSorterPoints.innerText = ('Poäng');
 
-  // Lägg till overlay till body
-	document.body.appendChild(statisticsOverlay);
+    // Skapa meny-section
+    const menuSection = document.createElement("section");
+    menuSection.className = ('menu-section')
 
-  // Lägg till listan till content
-	let users = JSON.parse(localStorage.getItem('users'));
-	let userList = document.createElement('ul');
-	userList.className = 'user-list';
-	users.forEach(user => {
-    let userItem = document.createElement('li');
-	userItem.className = 'user-item';
-    userItem.textContent = `${user.name} - Score: ${user.score}, Result: ${user.result}`;
-    userList.appendChild(userItem);
-	});
-	statisticsContent.appendChild(userList);
+    // skapa ul och sortera på senaste
+    let userList = document.createElement('ul');
+    userList.className = 'user-list';
+    users.sort((a, b) => (a.name < b.name) ? 1 : -1); // sort by latest
+    users.forEach(user => {
+        let userItem = document.createElement('li');
+        userItem.className = 'user-item';
+        userItem.textContent = `${user.name} - Score: ${user.score}, Result: ${user.result}`;
+        userList.appendChild(userItem);
+    });
 
-	statisticsOverlay.addEventListener("click", function (event) {
-    if (event.target === statisticsOverlay) {
-    	statisticsOverlay.remove();
-    }
-	});
+    // lägg till element till content
+    statisticsContent.appendChild(statisticsHeader);
+    statisticsContent.appendChild(menuSection);
+    menuSection.appendChild(statisticsSorterName);
+    menuSection.appendChild(statisticsSorterPoints);
+    statisticsContent.appendChild(userList);
+    statisticsOverlay.appendChild(statisticsContent);
+    document.body.appendChild(statisticsOverlay);
+
+    // event listener för att släcka ner overlay
+    statisticsOverlay.addEventListener("click", function (event) {
+        if (event.target === statisticsOverlay) {
+            statisticsOverlay.remove();
+        }
+    });
+
+    // event listener för att sortera efter poäng
+    statisticsSorterPoints.addEventListener("click", function (event) {
+        event.preventDefault();
+        users.sort((a, b) => (a.score < b.score) ? 1 : -1); // sort by points
+        userList.innerHTML = '';
+        users.forEach(user => {
+            let userItem = document.createElement('li');
+            userItem.className = 'user-item';
+            userItem.textContent = `${user.name} - Score: ${user.score}, Result: ${user.result}`;
+            userList.appendChild(userItem);
+        });
+    });
+
+    // event listener för att sortera efter senaste
+    statisticsSorterName.addEventListener("click", function (event) {
+        event.preventDefault();
+        users.sort((a, b) => (a.name < b.name) ? 1 : -1); // sort by latest
+        userList.innerHTML = '';
+        users.forEach(user => {
+            let userItem = document.createElement('li');
+            userItem.className = 'user-item';
+            userItem.textContent = `${user.name} - Score: ${user.score}, Result: ${user.result}`;
+            userList.appendChild(userItem);
+        });
+    });
 
 });
